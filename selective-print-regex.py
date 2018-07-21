a7318a4ff096ec85a96ef33eb7abc520076477f4
Sean O'Leary <sean@soleary.com>
2018-07-20 21:52:47 -0400

    so .match() only works at the beginning of a string

--- selective-print-regex.py
+++ selective-print-regex.py
@@ -2,10 +2,12 @@
 
 import re
 
-pat = re.compile('^br')
+print("So .match() matches against the beginning of the string, so we can leave out the '^' to anchor it")
+print("Note:  I'm not 100% sure this is *same* as '^', but it appears to work that way.")
+pat1 = re.compile('bro')
 
 with open('bros.txt', 'r') as f:
     for line in f:
-        if pat.match(line):
+        if pat1.match(line):
             print(line, end='')
 
