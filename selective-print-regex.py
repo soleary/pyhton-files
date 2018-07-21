0e5f175b34b1b9d2c1e84efa5831f8e47e746838
Sean O'Leary <sean@soleary.com>
2018-07-20 22:39:52 -0400

    Do the printing a bit better

--- selective-print-regex.py
+++ selective-print-regex.py
@@ -2,8 +2,9 @@
 
 import re
 
-print("So .match() matches against the beginning of the string, so we can leave out the '^' to anchor it")
-print("Note:  I'm not 100% sure this is *same* as '^', but it appears to work that way.")
+print("""So .match() matches against the beginning of the string, so we can
+leave out the '^' to anchor it. Note:  I'm not 100% sure this is *same* as
+'^', but it appears to work that way.""")
 pat1 = re.compile('bro')
 
 with open('bros.txt', 'r') as f:
@@ -11,9 +12,11 @@ with open('bros.txt', 'r') as f:
         if pat1.match(line):
             print(line, end='')
 
-print("\n.fullmatch() matches against the whole string, as if the supplied regex were anchored at each end.")
-print("It's not quite like '^regex$', as '$' will match a newline chracter at the end of string, but .fullmatch()")
-print("requires us to include the '\\n' in the regex.")
+print("""
+.fullmatch() matches against the whole string, as if the supplied regex were
+anchored at each end.  It's not quite like '^regex$', as '$' will match a
+newline chracter at the end of string, but .fullmatch() requires us to
+include the '\\n' in the regex.""")
 pat2 = re.compile('bro\n')
 
 with open('bros.txt', 'r') as f:
@@ -21,8 +24,9 @@ with open('bros.txt', 'r') as f:
         if pat2.fullmatch(line):
             print(line, end='')
 
-print("\n.search() will match anywhere in the supplied string, so if you only want to match at the beginning, you've got")
-print("to anchor the patten in your regex.")
+print("""
+.search() will match anywhere in the supplied string, so if you  only want to
+match at the beginning, you've got to anchor the patten in your regex.""")
 pat3 = re.compile('^bro')
 
 with open('bros.txt', 'r') as f:
@@ -30,8 +34,10 @@ with open('bros.txt', 'r') as f:
         if pat3.search(line):
             print(line, end='')
 
-print("\nThis will have to match a whole string, since it's anchored at both ends.  But '$' will match the end of a string or a newline")
-print("followed by the end of the string, so we don't have to actually include the '\\n' in our regex.")
+print("""
+This will have to match a whole string, since it's anchored at both ends, But
+'$' will match the end of a string or a newline followed by the end of the
+string, so we don't have to actually include the '\\n' in our regex.""")
 pat4 = re.compile('^bro$')
 
 with open('bros.txt', 'r') as f:
@@ -39,7 +45,9 @@ with open('bros.txt', 'r') as f:
         if pat4.search(line):
             print(line, end='')
 
-print("\nAnd here's .search() with an unanchored regex, so it can match at any place in the string.")
+print("""
+And here's .search() with an unanchored regex, so it can match at any place
+in the string.""")
 pat5 = re.compile('bro')
 
 with open('bros.txt', 'r') as f:
